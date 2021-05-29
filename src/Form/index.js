17b325fe8f99ea9036e6d78a0532b8544bb16438
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-05-29 18:09:00 +0200

    button text changed

--- src/Form/index.js
+++ src/Form/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Form = () => (
     <form className="form js-form">
             <input className="form__text" type="text" placeholder="What do you have to do?" />
-            <button className="button button--add">Click to add</button>
+            <button className="button button--add">Add new task</button>
           </form>
 )
 
