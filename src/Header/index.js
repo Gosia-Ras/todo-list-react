ee2e4b6f7252cc54d918670e1f89f89716d142d3
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-05-29 18:00:12 +0200

    unused css classes removed

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ title }) => (
     <header>
-        <h1 className="body__header">{title}</h1>
+        <h1>{title}</h1>
       </header>
 );
 
--- src/Section/index.js
+++ src/Section/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <div className="section__layout">
-            <h2 className="section__header">{title}</h2>
+            <h2>{title}</h2>
             {extraHeaderContent}
         </div>
         <div className="section__body">
