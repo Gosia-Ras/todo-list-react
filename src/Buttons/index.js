491d2bce5f6eb0affb3cdc5b7aa0b0f90d28f53e
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-06-17 12:29:22 +0200

    hideDoneTasks & hideAllDone renamed hideDone

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./style.css";
 
-const Buttons = ({ tasks, hideAllDone }) => (
+const Buttons = ({ tasks, hideDone }) => (
     tasks.length > 0 && (
         <div className="section__buttonsContainer">
             {tasks.length > 0 && (
                 <>
                     <button className={`button button--section $     {tasks.every(({ done }) => !done) ? "disabled" : ""}`}
                     >
-                    {hideAllDone ? "Show" : "Hide"} finished tasks
+                    {hideDone ? "Show" : "Hide"} finished tasks
                     </button>
                     <button 
                         className="button button--section"
