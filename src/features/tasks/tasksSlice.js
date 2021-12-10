8a05c834680ab0786a20a8e33aae9bf4e56b7f44
Gosia Ras <ras.malgorzata@gmail.com>
2021-12-10 11:49:05 +0100

    removeTask added to reducer

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -10,15 +10,19 @@ const tasksSlice = createSlice({
       tasks.push(payload);
     },
     toggleHideDone: state => {
-      state.hideDone = !state.hideDone;
+      state.hideDone = !state.hideDone
     },
-    toggleTaskDone: (state,action) => {
+    toggleTaskDone: ( state, action ) => {
       const index = state.tasks.findIndex(task => task.id === action.payload);
       state.tasks[index].done = !state.tasks[index].done
     },
+    removeTask: ({tasks}, {payload: deleteTask}) => {
+      const index = tasks.findIndex(({ id }) => id === deleteTask);
+      tasks.splice(index, 1);
+    }
   },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask } = tasksSlice.actions;
 export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
