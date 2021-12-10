08010bdb8d5e23cd610540877169f39ed5dc2213
Gosia Ras <ras.malgorzata@gmail.com>
2021-12-10 12:30:11 +0100

    code cleaned

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyle";
 import store from "./store";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
