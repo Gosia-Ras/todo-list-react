c8cd870fe13158042bee31707e9a3e529ac322b3
Gosia Ras <ras.malgorzata@gmail.com>
2022-01-16 17:24:19 +0100

    styling changed

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -4,9 +4,8 @@ export const StyledContainer = styled.main`
   max-width: 800px;
   margin: 0 auto;
   padding: 10px;
-  height: 100vh;
 
   @media (max-width: 767px) {
-   height: max-content;
+    height: max-content;
   }
 `;
--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -6,10 +6,6 @@ export const StyledFooter = styled.footer`
   padding: 10px;
   font-size: small;
   font-style: italic;
-  margin-top: 30px;
+  margin-top: 50px;
   color: ${({ theme }) => theme.color.quaternary};
-
-  @media (max-width: 767px) {
-    margin-top: 130px;
-   }
 `;
